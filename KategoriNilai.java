d91e7f71f78e3138e173a21279c29fc366b6c685
nadhifyulianto <nadhifyulianto999@gmail.com>
2025-08-20 08:24:46 +0700

    Add files via upload

--- KategoriNilai.java
+++ KategoriNilai.java
@@ -6,14 +6,28 @@ public class KategoriNilai {
         System.out.println("Masukkan nilai Anda:");
         int nilai = input.nextInt();
 
-        if (nilai >= 90 && nilai <= 100) {
+        if (nilai >= 96 && nilai <= 100) {
+            System.out.println("A+");
+        }   else if (nilai >= 95 && nilai <= 91) {
             System.out.println("A");
-        }   else if (nilai >= 70 && nilai <= 89) {
+        }   else if (nilai >= 86 && nilai <= 90) {
+            System.out.println("B+");
+        }   else if (nilai >= 81 && nilai <= 85) {
             System.out.println("B");
-        }   else if (nilai >= 50 && nilai <= 69) {
+        }   else if (nilai >= 75 && nilai <= 80) {
+            System.out.println("B-");
+        }   else if (nilai >= 70 && nilai <= 74) {
+            System.out.println("C+");
+        }   else if (nilai >= 65 && nilai <= 69) {
             System.out.println("C");
-        }   else if (nilai <= 49) {
+        }   else if (nilai >= 60 && nilai <= 64) {
+            System.out.println("C-");
+        }   else if (nilai >= 55 && nilai <= 59) {
+            System.out.println("D+");
+        }   else if (nilai >= 0 && nilai <= 54 ) {
             System.out.println("D");
         }
+    }
 }
-}
+
+
